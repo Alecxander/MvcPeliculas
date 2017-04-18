f391facbf4c34f783d05b7fc47bcc93b282281ea
Alexander Puin <cdniup@gmail.com>
2017-04-18 08:46:34 -0500

    Primer commit MvcPeliculas

---
+++ MvcPeliculas/Data/MvcPeliculasContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcPeliculas.Models
+{
+    public class MvcPeliculasContext : DbContext
+    {
+        public MvcPeliculasContext (DbContextOptions<MvcPeliculasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcPeliculas.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ MvcPeliculas/Migrations/20170405132421_Initial.Designer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MvcPeliculas.Models;
+
+namespace MvcPeliculas.Migrations
+{
+    [DbContext(typeof(MvcPeliculasContext))]
+    [Migration("20170405132421_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("MvcPeliculas.Models.Movie", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Genre");
+
+                    b.Property<decimal>("Price");
+
+                    b.Property<DateTime>("ReleaseDate");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Movie");
+                });
+        }
+    }
+}
